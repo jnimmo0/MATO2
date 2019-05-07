d49127407104f37cc22063b6e522aa2cd545210d
Joshua Nimmo 2020 <jnimmo0@3012-l29532-s.local>
2019-05-07 09:51:50 -0500

    Initial commit

--- MATO2/ViewController.swift
+++ MATO2/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  MATO2
+//  MATO
 //
 //  Created by Joshua Nimmo 2020 on 5/7/19.
 //  Copyright © 2019 Joshua Nimmo 2020. All rights reserved.
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var board:[Int]=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    let placer:[Int]=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var cplacer=placer
+        while cplacer.count != 0{
+            var index=(Int)(arc4random_uniform(UInt32(board.count)))
+            if(board[index]==0){
+                board[index]=cplacer.remove(at: 0)
+            }
+        }
+        for x in board{
+            
+        }
     }
-
-
+    
+    
 }
 
