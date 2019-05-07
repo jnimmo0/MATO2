7ce2436f5c1256d79cad300dce198f8770de86af
Joshua Nimmo 2020 <jnimmo0@3012-l29532-s.local>
2019-05-07 09:48:29 -0500

    Initial Commit

---
+++ MATO2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MATO2
+//
+//  Created by Joshua Nimmo 2020 on 5/7/19.
+//  Copyright © 2019 Joshua Nimmo 2020. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
